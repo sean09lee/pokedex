00deddef31323d7745c76c237a93a6dbd91c139c
Sean Lee <seanl@interknowlogy.com>
2019-02-28 10:41:56 -0800

    Add persistent storage and offline localhost mode

--- README.md
+++ README.md
@@ -4,11 +4,16 @@ This Pokedex applications was built to test the offline capabilities of both the
 
 ## Running locally
 
-You can run this project in the web or as an electron application.
+You can run this project in the web or as an electron application. Note that running in the web with service workers does not work with hot reloading. So, if you want to see changes with service, you will need to rebuild and then refresh.
 
-**Run in the web:**
+**Run in the web (no service workers):**
 ```
 npm run web
+```
+
+**Run with service workers enabled:**
+```
+npm run offline
 ```
 
 **Run via electron**
--- src/app/database/pokemon.database.ts
+++ src/app/database/pokemon.database.ts
@@ -46,7 +46,71 @@ export class PokemonDexie extends Dexie {
         pokemon: 'id,name,image'
       });
       this.pokemon = this.table('pokemon');
+      this.initStoragePersistence();
+      this.showEstimatedQuota();
   }
 
-}
+  private async persist(): Promise<boolean> {
+    return await navigator.storage && navigator.storage.persist &&
+      navigator.storage.persist();
+  }
 
+  public async isStoragePersisted(): Promise<boolean> {
+    return await navigator.storage && navigator.storage.persist &&
+      navigator.storage.persisted();
+  }
+
+  private async initStoragePersistence() {
+    const persist = await this.tryPersistWithoutPromtingUser();
+    switch (persist) {
+      case 'never':
+        console.log('Not possible to persist storage');
+        break;
+      case 'persisted':
+        console.log('Successfully persisted storage silently');
+        break;
+      case 'prompt':
+        console.log('Not persisted, but we may prompt user when we want to.');
+        break;
+    }
+  }
+
+  private async tryPersistWithoutPromtingUser() {
+    if (!navigator.storage || !navigator.storage.persisted) {
+      return 'never';
+    }
+    let persisted = await navigator.storage.persisted();
+    if (persisted) {
+      return 'persisted';
+    }
+    const nav: any = window.navigator;
+    if (!nav.permissions || !nav.permissions.query) {
+      return 'prompt'; // It MAY be successful to prompt. Don't know.
+    }
+    const permission: any = await nav.permissions.query({
+      name: 'persistent-storage'
+    });
+    if (permission.status === 'granted') {
+      persisted = await navigator.storage.persist();
+      if (persisted) {
+        return 'persisted';
+      } else {
+        throw new Error('Failed to persist');
+      }
+    }
+    if (permission.status === 'prompt') {
+      return 'prompt';
+    }
+    return 'never';
+  }
+
+  public async showEstimatedQuota(): Promise<void> {
+    if (navigator.storage && navigator.storage.estimate) {
+      const estimation = await navigator.storage.estimate();
+      console.log(`Quota: ${estimation.quota}`);
+      console.log(`Usage: ${estimation.usage}`);
+    } else {
+      console.error('StorageManager not found');
+    }
+  }
+}
